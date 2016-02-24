ceb29d2e5af193c707cec20dc2242cd039cbdc79
oarchner <oliver.archner@uni-bayreuth.de>
2016-02-24 15:03:09 +0100

    Refactored imports

--- src/test/java/de/unibayreuth/bayceer/bayeos/xmlprc/handler/TestTreeHandler.java
+++ src/test/java/de/unibayreuth/bayceer/bayeos/xmlprc/handler/TestTreeHandler.java
@@ -1,17 +1,12 @@
 package de.unibayreuth.bayceer.bayeos.xmlprc.handler;
 
 
-import java.io.IOException;
 import java.util.Vector;
 
 import org.apache.xmlrpc.XmlRpcException;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
-import de.unibayreuth.bayceer.bayeos.client.AbstractClientTest;
 import de.unibayreuth.bayceer.bayeos.client.NodeTest;
-import de.unibayreuth.bayceer.bayeos.objekt.ObjektArt;
 import de.unibayreuth.bayceer.bayeos.objekt.ObjektNode;
 
 public class TestTreeHandler extends NodeTest {
