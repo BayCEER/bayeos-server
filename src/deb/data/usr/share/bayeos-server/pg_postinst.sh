60ec26822388729a71ca2f4885d698f4f10fbaf6
oliver archner <oliver.archner@uni-bayreuth.de>
2022-06-09 09:13:44 +0200

    Upgrade flyway table name

--- src/deb/data/usr/share/bayeos-server/pg_postinst.sh
+++ src/deb/data/usr/share/bayeos-server/pg_postinst.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 set -e
-
 PRG="$0"
 INSTALLDIR=`dirname "$PRG"`
 
@@ -12,7 +11,10 @@ then
  psql -c "ALTER ROLE bayeos PASSWORD '4336bc9de7a6b11940e897ee22956d51'" 2>&1
  createdb -O bayeos -E UTF-8 bayeos 2>&1
 fi
+
+echo "Try to upgrade flyway table name"
+psql -d bayeos -c "alter table if exists public.schema_version rename TO public.flyway_schema_history;" 2>&1
+
 psql -c "ALTER ROLE bayeos SUPERUSER" 2>&1
-psql -d bayeos -c "ALTER TABLE if exists public.schema_version SET SCHEMA bayeos;" 2>&1
-java -Djava.security.egd=file:/dev/../dev/urandom -cp $INSTALLDIR/lib/*:$INSTALLDIR/drivers/* org.flywaydb.commandline.Main -url=${db.url} -driver=org.postgresql.Driver -user=${db.user} -password=${db.password} -validateOnMigrate=false -baselineOnMigrate=true -locations=filesystem:$INSTALLDIR/sql migrate
+java -Djava.security.egd=file:/dev/../dev/urandom -cp $INSTALLDIR/lib/*:$INSTALLDIR/drivers/* org.flywaydb.commandline.Main -url=${db.url} -driver=org.postgresql.Driver -user=${db.user} -password=${db.password} -locations=filesystem:$INSTALLDIR/sql migrate
 psql -c "ALTER ROLE bayeos NOSUPERUSER" 2>&1
